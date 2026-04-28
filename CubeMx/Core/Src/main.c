41bfbb543bbb0ff7db58c58fe44b7ec2de789cbc
Nicolai Neuber <info@nicolai-neuber.de>
2026-04-28 15:30:55 +0200

    no message soory

--- CubeMx/Core/Src/main.c
+++ CubeMx/Core/Src/main.c
@@ -1,5 +1,5 @@
 /* USER CODE BEGIN Header */
-/**
+/** Nico
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
